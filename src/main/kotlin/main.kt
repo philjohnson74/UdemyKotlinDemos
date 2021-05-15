5ad95d2519a063dc51f5e8d2740c816e4f402990
philliptjohnson <philliptjohnson@hotmail.com>
2021-05-15 22:55:25 +0100

    Variables and printing lines

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val name = "Phil"
+    val age = 21
+
+    println("===== User Info =====")
+    println("Name: $name")
+    println("Age: $age")
+
+    var count = 1
+    println("===== Count Info =====")
+    println("Count = $count")
+    count = 10
+    println("Count = $count")
+
+    var department: String?
+    department = null
+    department = "Engineering"
+    println("===== Department Info =====")
+    println("Department: ${department!!}")
 }
